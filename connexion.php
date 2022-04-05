cee3b9dcdcfc62a95b1de13e8ec1dc8d2246d14f
AtnRic <antoine.richard@student.junia.com>
2022-04-05 12:20:01 +0200

    création page connexion (+responsive)

---
+++ connexion.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8" />
+    <meta name="description" content="Twotter">
+    <meta name="keywords" content="twotter">
+    <meta name="author" content="Antoine RICHARD, Adrien VERHAEGHE">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twotter - Connexion</title>
+    <link rel="stylesheet" href="css/connexion.css">
+</head>
+<body>
+    <div class="left">
+        <p>test</p>
+    </div>
+
+    <div class="right">
+        <div class="pad">
+            <h1>Ça se passe<br>maintenant.</h1>
+            <h2>Rejoignez Twotter dès aujourd'hui.</h2>
+            <br>
+            <a class="button" href="red-november.html">S'inscrire</a>
+            <br>
+            <h3>Vous avez déjà un compte ?</h3>
+            <a class="button" href="red-november.html">Se connecter</a>
+        </div>
+        
+    </div>
+</body>
+</html>
