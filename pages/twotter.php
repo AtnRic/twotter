5ac5f23a13005ca1a545a60bb8b1b8957433e26c
AtnRic <antoine.richard@student.junia.com>
2022-04-13 00:25:42 +0200

    sidebar

--- pages/twotter.php
+++ pages/twotter.php
@@ -7,22 +7,68 @@
     <meta name="author" content="Antoine RICHARD, Adrien VERHAEGHE">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Twotter - Twoots</title>
-    <link rel="stylesheet" href="../css/twotter-light.css">
+    <link rel="stylesheet" href="../css/twotter-css/twotter-light.css">
+    <link rel="stylesheet" href="../css/twotter-css/twotter.css">
     <link rel="icon" type="image/png" href="../images/bat.png" />
 </head>
 
 <body>
 <!--sidebar-->
-<div class="left">
-    <img src="../images/bat.png">
-    <div class="sidebar">
-        <a class="button" href=""><img src="../images/icons/home_black_24dp.svg" alt="Enregistré">Home</a><br>
-        <a class="button" href=""><img src="../images/icons/turned_in_not_black_24dp.svg" alt="Enregistré">Enregistré</a><br>
-        <a class="button" href=""><img src="../images/icons/account_circle_black_24dp.svg" alt="Profil">Profil</a><br>
-        <a class="button" href=""><img src="../images/icons/settings_black_24dp.svg" alt="Paramètres">Paramètres</a><br>
-        <a class="button" href=""><img src="../images/icons/logout_black_24dp.svg" alt="Déconnexion">Déconnexion</a><br>
+    <div class="left">
+        <img src="../images/bat.png" alt="logo">
+        <nav class="sidebar">
+            <a class="button" href="">
+                <div class="image_texte">
+                    <div class="image">
+                        <img src="../images/icons/home_black_24dp.svg" alt="Home">
+                    </div>
+                    <div class="texte">
+                        <span>Home</span>
+                    </div>
+                </div>
+            </a>
+            <a class="button" href="">
+                <div class="image_texte">
+                    <div class="image">
+                        <img src="../images/icons/turned_in_not_black_24dp.svg" alt="Enregistré">
+                    </div>
+                    <div class="texte">
+                        <span>Enregistré</span>
+                    </div>
+                </div>
+            </a>
+            <a class="button" href="">
+                <div class="image_texte">
+                    <div class="image">
+                        <img src="../images/icons/account_circle_black_24dp.svg" alt="Profil">
+                    </div>
+                    <div class="texte">
+                        <span>Profil</span>
+                    </div>
+                </div>
+            </a>
+            <a class="button" href="">
+                <div class="image_texte">
+                    <div class="image">
+                        <img src="../images/icons/settings_black_24dp.svg" alt="Paramètres">
+                    </div>
+                    <div class="texte">
+                        <span>Paramètres</span>
+                    </div>
+                </div>
+            </a>
+            <a class="button" href="">
+                <div class="image_texte">
+                    <div class="image">
+                        <img src="../images/icons/logout_black_24dp.svg" alt="Déconnexion">
+                    </div>
+                    <div class="texte">
+                        <span>Déconnexion</span>
+                    </div>
+                </div>
+            </a>
+        </nav>
     </div>
-</div>
     <div class="center">
         <div class="twoot">
             <p>Ecrire un twoot</p>
