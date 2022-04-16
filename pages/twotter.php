eca7488e5ce96c8ddf290f72e569ff1978203681
AtnRic <antoine.richard@student.junia.com>
2022-04-16 23:57:43 +0200

    dark theme en cours

--- pages/twotter.php
+++ pages/twotter.php
@@ -9,7 +9,7 @@
     <title>Twotter - Twoots</title>
     <script src="https://kit.fontawesome.com/235593db07.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/twotter-css/twotter.css">
-    <link rel="stylesheet" href="../css/twotter-css/twotter-light.css">
+    <link rel="stylesheet" href="../css/twotter-css/twotter-dark.css">
     <link rel="icon" type="image/png" href="../images/bat.png" />
 </head>
 <body>
@@ -38,7 +38,7 @@
                     <a href="">Accueil</a>
                 </span>
                 <span>
-                    <i class="star"></i>
+                    <i class="fa-solid fa-badge-check"></i>
                 </span>
             </div>
             <div class="you_tweet_other_tweet">
@@ -46,7 +46,6 @@
                     <div class="profil_message">
                         <span><img src="../images/bat.png" alt=""></span>
                         <span><label><input type="text" placeholder="Quoi de neuf ?"></label></span>
-
                     </div>
                     <span><button>Twoot</button></span>
                 </div>
@@ -59,20 +58,17 @@
                     <div class="profil_msg">
                         <div class="other_profile">
                             <!--photo profil-->
-                            <img src="../images/bat.png" alt="photo de profil">
+                            <img src="../images/pp/perceval.PNG" alt="photo de profil">
                         </div>
                         <div class="name_msg">
-                            <span><p><b>Prénom Nom</b><i class="check"></i>@pseudo <small>date</small></p></span>
+                            <span><p><b>Perceval de Galles</b><i class="fa-solid fa-badge-check"></i>@Gerceval <small>Today</small></p></span>
                             <div class="msg">
-                                <p>Lorem ipsum, dolor sit amet</p>
+                                <p>On en a gros !</p>
                             </div>
-                        </div>
-                        <div class="more_options">
-                            <i class="ellipsis"></i>
                         </div>
                     </div>
                     <div class="image_video">
-                        <img src="../images/image.png" alt="">
+                        <img src="" alt="">
                     </div>
                     <div class="your_reaction">
                         <div class="comment"><i class="fa-solid fa-comment"></i><p>12</p></div>
@@ -85,52 +81,23 @@
                     <div class="profil_msg">
                         <div class="other_profile">
                             <!--photo profil-->
-                            <img src="../images/bat.png" alt="photo de profil">
+                            <img src="../images/pp/arthur.PNG" alt="photo de profil">
                         </div>
                         <div class="name_msg">
-                            <span><p><b>Prénom Nom</b><i class="check"></i>@pseudo <small>date</small></p></span>
+                            <span><p><b>Arthur Roi de Bretagne</b><i class="fa-solid fa-badge-check"></i>@Arthur <small>Today</small></p></span>
                             <div class="msg">
-                                <p>Lorem ipsum, dolor sit amet</p>
+                                <p>Je suis roi de Bretagne, j'ai pas de conseil à recevoir d'une clodo !</p>
                             </div>
-                        </div>
-                        <div class="more_options">
-                            <i class="ellipsis"></i>
                         </div>
                     </div>
                     <div class="image_video">
-                        <img src="../images/image.png" alt="">
+                        <img src="../images/media/arthur.gif" alt="">
                     </div>
                     <div class="your_reaction">
-                        <div class="comment"><i class="fa-solid fa-comment"></i><p>12</p></div>
-                        <div class="retweet"><i class="fa-solid fa-retweet"></i><p>12</p></div>
-                        <div class="like"><i class="fa-solid fa-heart"></i><p>12</p></div>
-                        <div class="bookmark"><i class="fa-solid fa-bookmark"></i><p>12</p></div>
-                    </div>
-                </div>
-                <div class="other_tweet">
-                    <div class="profil_msg">
-                        <div class="other_profile">
-                            <!--photo profil-->
-                            <img src="../images/bat.png" alt="photo de profil">
-                        </div>
-                        <div class="name_msg">
-                            <span><p><b>Prénom Nom</b><i class="check"></i>@pseudo <small>date</small></p></span>
-                            <div class="msg">
-                                <p>Lorem ipsum, dolor sit amet</p>
-                            </div>
-                        </div>
-                        <div class="more_options">
-                            <i class="ellipsis"></i>
-                        </div>
-                    </div>
-                    <div class="image_video">
-                        <img src="../images/image.png" alt="">
-                    </div>
-                    <div class="your_reaction">
-                        <div class="comment"><i class="fa-solid fa-comment"></i><p>12</p></div>
-                        <div class="retweet"><i class="fa-solid fa-retweet"></i><p>12</p></div>
-                        <div class="like"><i class="fa-solid fa-heart"></i><p>12</p></div>
-                        <div class="bookmark"><i class="fa-solid fa-bookmark"></i><p>12</p></div>
+                        <div class="comment"><i class="fa-solid fa-comment"></i><p>35</p></div>
+                        <div class="retweet"><i class="fa-solid fa-retweet"></i><p>12k</p></div>
+                        <div class="like"><i class="fa-solid fa-heart"></i><p>18k</p></div>
+                        <div class="bookmark"><i class="fa-solid fa-bookmark"></i><p>20</p></div>
                     </div>
                 </div>
                 <div class="other_tweet">
@@ -177,17 +144,32 @@
                         <div class="trend">
                             <div class="trend_msg">
                                 <div class="trend_name">
+                                    <p>Kaamelott</p>
+                                </div>
+                                <div class="subject">
+                                    <p>Kaamelott</p>
+                                </div>
+                                <div class="trend_with">
+                                    <p>trending with : <br><a href="">#ProvencalLeGaulois</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <!--each trends-->
+                        <div class="trend">
+                            <div class="trend_msg">
+                                <div class="trend_name">
                                     <p>twoot trend</p>
                                 </div>
                                 <div class="subject">
                                     <p>Hello world (subject)</p>
                                 </div>
                                 <div class="trend_with">
-                                    <p>trending with <a href="">#Machinàremplacer</a>,<a href="">#AutreTrucARenplacer</a> </p>
+                                    <p>trending with : <br><a href="">#Machinàremplacer</a>, <a href="">#AutreTrucARenplacer</a></p>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                     <div class="show_more">
                         <a href="">En voir plus</a>
                     </div>
