f5daea41a0a77914b3f822c3583dd7eabac186bd
AtnRic <antoine.richard@student.junia.com>
2022-04-17 23:58:11 +0200

    kjhk

--- pages/twotter.php
+++ pages/twotter.php
@@ -58,12 +58,58 @@
                     <div class="profil_msg">
                         <div class="other_profile">
                             <!--photo profil-->
-                            <img src="../images/pp/perceval.PNG" alt="photo de profil">
+                            <img src="../images/pp/karadoc.PNG" alt="photo de profil">
                         </div>
                         <div class="name_msg">
-                            <span><p><b>Perceval de Galles</b><i class="fa-solid fa-badge-check"></i>@Gerceval <small>Today</small></p></span>
+                            <span><p><b>Karadoc de Vannes</b><i class="fa-solid fa-badge-check"></i>@Karadoc <small>Today</small></p></span>
                             <div class="msg">
-                                <p>On en a gros !</p>
+                                <p>Le CSS :</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="image_video">
+                        <img src="../images/media/trouver-utilisation-perimetrique.gif" alt="">
+                    </div>
+                    <div class="your_reaction">
+                        <div class="comment"><i class="fa-solid fa-comment"></i><p>12</p></div>
+                        <div class="retweet"><i class="fa-solid fa-retweet"></i><p>12</p></div>
+                        <div class="like"><i class="fa-solid fa-heart"></i><p>12</p></div>
+                        <div class="bookmark"><i class="fa-solid fa-bookmark"></i><p>12</p></div>
+                    </div>
+                </div>
+                <div class="other_tweet">
+                    <div class="profil_msg">
+                        <div class="other_profile">
+                            <!--photo profil-->
+                            <img src="../images/pp/arthur.PNG" alt="photo de profil">
+                        </div>
+                        <div class="name_msg">
+                            <span><p><b>Arthur Roi de Bretagne</b><i class="fa-solid fa-badge-check"></i>@Arthur <small>Today</small></p></span>
+                            <div class="msg">
+                                <p>Ne m'appelez pas Sire !</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="image_video">
+                        <img src="../images/media/terme-elegant.gif" alt="">
+                    </div>
+                    <div class="your_reaction">
+                        <div class="comment"><i class="fa-solid fa-comment"></i><p>35</p></div>
+                        <div class="retweet"><i class="fa-solid fa-retweet"></i><p>12k</p></div>
+                        <div class="like"><i class="fa-solid fa-heart"></i><p>18k</p></div>
+                        <div class="bookmark"><i class="fa-solid fa-bookmark"></i><p>20</p></div>
+                    </div>
+                </div>
+                <div class="other_tweet">
+                    <div class="profil_msg">
+                        <div class="other_profile">
+                            <!--photo profil-->
+                            <img src="../images/pp/burgonde.PNG" alt="photo de profil">
+                        </div>
+                        <div class="name_msg">
+                            <span><p><b>Roi Burgonde</b><i class="check"></i>@Rburgonde <small>Today</small></p></span>
+                            <div class="msg">
+                                <p>J'apprécie des fruits au sirop</p>
                             </div>
                         </div>
                     </div>
@@ -84,40 +130,37 @@
                             <img src="../images/pp/arthur.PNG" alt="photo de profil">
                         </div>
                         <div class="name_msg">
-                            <span><p><b>Arthur Roi de Bretagne</b><i class="fa-solid fa-badge-check"></i>@Arthur <small>Today</small></p></span>
+                            <span><p><b>Arthur Roi de Bretagne</b><i class="check"></i>@Arthur <small>Today</small></p></span>
                             <div class="msg">
-                                <p>Je suis roi de Bretagne, j'ai pas de conseil à recevoir d'une clodo !</p>
+                                <p>Quand tu merge 2 branches, git be like :</p>
                             </div>
                         </div>
                     </div>
                     <div class="image_video">
-                        <img src="../images/media/arthur.gif" alt="">
+                        <img src="../images/media/le-nouveau-code.gif" alt="">
                     </div>
                     <div class="your_reaction">
-                        <div class="comment"><i class="fa-solid fa-comment"></i><p>35</p></div>
-                        <div class="retweet"><i class="fa-solid fa-retweet"></i><p>12k</p></div>
-                        <div class="like"><i class="fa-solid fa-heart"></i><p>18k</p></div>
-                        <div class="bookmark"><i class="fa-solid fa-bookmark"></i><p>20</p></div>
+                        <div class="comment"><i class="fa-solid fa-comment"></i><p>12</p></div>
+                        <div class="retweet"><i class="fa-solid fa-retweet"></i><p>12</p></div>
+                        <div class="like"><i class="fa-solid fa-heart"></i><p>12</p></div>
+                        <div class="bookmark"><i class="fa-solid fa-bookmark"></i><p>12</p></div>
                     </div>
                 </div>
                 <div class="other_tweet">
                     <div class="profil_msg">
                         <div class="other_profile">
                             <!--photo profil-->
-                            <img src="../images/bat.png" alt="photo de profil">
+                            <img src="../images/pp/burgonde.PNG" alt="photo de profil">
                         </div>
                         <div class="name_msg">
-                            <span><p><b>Prénom Nom</b><i class="check"></i>@pseudo <small>date</small></p></span>
+                            <span><p><b>Roi Burgonde</b><i class="check"></i>@Rburgonde <small>Today</small></p></span>
                             <div class="msg">
-                                <p>Lorem ipsum, dolor sit amet</p>
+                                <p>Git quand il y a un conflit de version :</p>
                             </div>
-                        </div>
-                        <div class="more_options">
-                            <i class="ellipsis"></i>
                         </div>
                     </div>
                     <div class="image_video">
-                        <img src="../images/image.png" alt="">
+                        <img src="../images/media/quest-ce-a-dire-que-ceci.gif" alt="">
                     </div>
                     <div class="your_reaction">
                         <div class="comment"><i class="fa-solid fa-comment"></i><p>12</p></div>
